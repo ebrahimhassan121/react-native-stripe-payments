7ddafb989535feb5b0e1d89f1b6c4793e8b80055
ebrahimhassan121 <ebrahimhassan121@gmail.com>
2021-05-04 17:19:06 +0200

    fix import stripe

--- ios/StripePayments.h
+++ ios/StripePayments.h
@@ -1,6 +1,7 @@
 #import <Foundation/Foundation.h>
 #import <React/RCTBridgeModule.h>
-#import <Stripe/Stripe.h>
+// #import <Stripe/Stripe.h>
+@import Stripe;
 
 @interface StripePayments : NSObject<RCTBridgeModule, STPAuthenticationContext>
 
